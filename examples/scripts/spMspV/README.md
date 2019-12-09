d1fc0b65dd293ee1eec1905c62ff32727bec68c0
Guowei Zhang <Guowei@31-34-170.wireless.csail.mit.edu>
2019-12-09 17:40:12 -0500

    Add merge-based SpMSpV example, which uses Fiber.swapRanks()

--- examples/scripts/spMspV/README.md
+++ examples/scripts/spMspV/README.md
@@ -1,5 +1,7 @@
-# spMspV has two styles
+# spMspV styles
 
 * C-stationary, i.e., output-stationary, A-stationary row-major, inner-product style
 
 * B-stationary, i.e., A-stationary column-major, outer-product style 
+
+* Merge-based, which uses Fiber.swapRank() function
---
+++ examples/scripts/spMspV/merge-based.py
@@ -0,0 +1,45 @@
+
+import os
+
+from fibertree import Tensor
+
+#
+# Merge-based matrix-vector multiply
+#
+
+print("--------------------------------------")
+print("      Merge-based spMspV")
+print("--------------------------------------")
+print("")
+
+data_dir = "../../data"
+
+a = Tensor(os.path.join(data_dir, "spMspV-a-t.yaml"))
+b = Tensor(os.path.join(data_dir, "spMspV-b.yaml"))
+
+z = Tensor(rank_ids=["M"])
+
+a.print("A Tensor")
+b.print("B Tensor")
+z.print("Z Tensor")
+
+a_k = a.root()
+b_k = b.root()
+z_m = z.root()
+
+a_k.print("A Tensor - Rank K")
+b_k.print("B Tensor - Rank K")
+z_m.print("Z Tensor - Rank M")
+
+ab = a_k & b_k
+ab_m = ab.swapRanks()
+
+for m_coord, (z_ref, ab_k) in z_m << ab_m:
+    for k_coord, (a_val, b_val) in ab_k:
+        z_ref += a_val * b_val # reducing a scalar
+
+z.print("\nZ Tensor")
+
+print("")
+print("--------------------------------------")
+print("")
