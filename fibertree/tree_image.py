d44f8227450ddee5e47ab48c933e3dc833e09035
Joel Emer <emer@csail.mit.edu>
2020-06-18 08:39:45 -0400

    Bug fix in TreeImage display
    Display of tensors with many empty fibers were getting truncated.
    
    Also fixed a number of Python style bugs

--- fibertree/tree_image.py
+++ fibertree/tree_image.py
@@ -4,9 +4,9 @@ from fibertree import Tensor
 from fibertree import Fiber
 from fibertree.payload import Payload
 
+
 class TreeImage():
     """TreeImage"""
-
 
     def __init__(self, object, highlights={}, extent=(30, 200)):
         """__init__
@@ -99,9 +99,9 @@ class TreeImage():
             #
             # Create rank_id string
             #
-            # Note: a rank_id may be a list that needs to be convert to a string
+            # Note: if rank_id is a list, convert to a string
             #
-            ranks = ", ".join([ str(r) for r in object.getRankIds()])
+            ranks = ", ".join([str(r) for r in object.getRankIds()])
 
             if name:
                 self.draw_rank(0, f"Tensor: {name}[{ranks}]")
@@ -153,7 +153,12 @@ class TreeImage():
 #
 # Method to traverse (and draw) all the levels of the tree
 #
-    def traverse(self, fiber, level=0, offset=0, highlights={}, highlight_subtensor={}):
+    def traverse(self,
+                 fiber,
+                 level=0,
+                 offset=0,
+                 highlights={},
+                 highlight_subtensor={}):
         """traverse"""
         #
         # Check if this is level0, which may just be a payload
@@ -173,7 +178,12 @@ class TreeImage():
                 #
                 # Recursively traverse and draw the fibers of a non-0-D tensor
                 #
-                region_end = self.traverse(fiber, level=1, offset=offset, highlights=highlights, highlight_subtensor=highlight_subtensor)
+                region_end = self.traverse(fiber,
+                                           level=1,
+                                           offset=offset,
+                                           highlights=highlights,
+                                           highlight_subtensor=highlight_subtensor)
+
                 region_size = region_end - region_start
                 #
                 # Draw root of tree
@@ -206,7 +216,7 @@ class TreeImage():
         #
         targets = []
         coordinate_start = region_start
-        
+
         #
         # Set up the highlighting for this level
         #
@@ -217,28 +227,32 @@ class TreeImage():
         highlight_coords = {}
 
         for worker, points in highlights.items():
-            highlight_coords[worker] = [c[0] for c in points]
+            highlight_coords[worker] = [pt[0] for pt in points]
 
         #
         # Traverse the fiber at this level
         #
         for n, (c, p) in enumerate(fiber):
-#            if n > 10: break
+            #
+            # TBD: Truncate fibers with too many elements
+            #
+            # if n > 10: break
 
             if Payload.contains(p, Fiber):
                 #
                 # The payload is a fiber so we need to recurse, but we
                 # also need to figure out what to highlight at the
                 # next level So these variables hold the highlight
-                # information with one less coordinate, and a list of
-                # workers that are highlighting the remaining
-                # subtensor
+                # information with one less coordinate (in
+                # "highlight_next") for each worker, and a dictionary
+                # of workers (in "highlight_subtensor_next") that are
+                # highlighting the remaining subtensor
                 #
                 highlight_next = {}
                 highlight_subtensor_next = {}
 
                 for worker, points in highlights.items():
-                    highlight_next[worker] = [ p[1:] for p in points if len(p) > 1 and p[0] == c ]
+                    highlight_next[worker] = [ pt[1:] for pt in points if len(pt) > 1 and pt[0] == c ]
                     #
                     # Once we start highlighting a fiber, highlight the entire subtensor.
                     # TBD: Maybe we should have just copied highlight_subtensor
@@ -247,23 +261,38 @@ class TreeImage():
                         highlight_subtensor_next[worker] = True
 
                     #
-                    # If there are no more coordinates, maybe start highlighting a subtensor 
+                    # If there are no more coordinates,
+                    # maybe start highlighting a subtensor
                     #
                     if len(highlight_next[worker]) == 0 and c in highlight_coords[worker]:
                         highlight_subtensor_next[worker] = True
 
-
-                region_end = self.traverse(Payload.get(p), level+1, region_end, highlight_next, highlight_subtensor_next)
+                #
+                # Draw the object below this coordinate (in "c")
+                #
+                region_end = self.traverse(Payload.get(p),
+                                           level+1,
+                                           region_end,
+                                           highlight_next,
+                                           highlight_subtensor_next)
             else:
                 region_end += 1
 
+            #
+            # Record (in "targets") the middle of the object below
+            # this coordinate to draw a line to it later, and
+            # calculate where the next object starts ("coordinate_start")
+            #
             targets.append(coordinate_start+(region_end-coordinate_start)/2)
             coordinate_start = region_end
 
+        #
+        # If the fiber was empty we still occupy a single space
+        #
+        if len(fiber) == 0:
+            region_end += 1
 
         region_size = region_end - region_start
-
-
 
         #
         # Display fiber for this level
@@ -271,11 +300,14 @@ class TreeImage():
         fiber_size = len(fiber)
         fiber_start = region_start + (region_size - fiber_size)/2
 
-        self.draw_fiber(level, fiber_start, fiber_start+fiber_size, highlight_subtensor)
+        self.draw_fiber(level,
+                        fiber_start,
+                        fiber_start+fiber_size,
+                        highlight_subtensor)
 
-        pos=fiber_start
+        pos = fiber_start
 
-        for c,p in fiber:
+        for c, p in fiber:
             #
             # Create sets of workers to be colored
             #
@@ -289,15 +321,30 @@ class TreeImage():
             self.draw_coord(level, pos, c, color_coord_or_subtensor)
 
             if len(color_coord - color_subtensor):
-                self.draw_intra_line(level, fiber_start + fiber_size / 2, pos+0.5, True)
+                self.draw_intra_line(level,
+                                     fiber_start + fiber_size / 2,
+                                     pos+0.5,
+                                     True)
 
-            # Only draw the line if the next level will actually draw something.
+            #
+            # Draw the line if the next level will actually draw something.
+            #
             if not Payload.contains(p, Fiber) or not p.isEmpty():
-                self.draw_line(level, pos+0.5, level+1, targets.pop(0), len(color_coord_or_subtensor)>0)
+                self.draw_line(level,
+                               pos+0.5,
+                               level+1,
+                               targets.pop(0),
+                               len(color_coord_or_subtensor) > 0)
 
             if not Payload.contains(p, Fiber):
-                # How could this not be the leaf --- "and rest_of_highlighting == []"
-                self.draw_value(level+1, pos, Payload.get(p), color_coord_or_subtensor)
+                #
+                # How could this not be the leaf ---
+                # "and rest_of_highlighting == []"
+                #
+                self.draw_value(level+1,
+                                pos,
+                                Payload.get(p),
+                                color_coord_or_subtensor)
 
             pos += 1
 
@@ -310,13 +357,20 @@ class TreeImage():
 
         # Constrain image size (overage matches crop above)
 
-        x_pixels = self.offset2x(self.col_extent+1) + 200 # was 8192
-        y_pixels = self.level2y(self.row_extent+1) + 20 # was 1024
+        #
+        # Size used to be (8192, 1024)
+        #
+        x_pixels = self.offset2x(self.col_extent+1) + 200
+        y_pixels = self.level2y(self.row_extent+1) + 20
 
+        #
         # Create an image at least this tall (in pixels)
+        #
         self.max_y = 100
 
+        #
         # Do image related setup
+        #
         self.im = Image.new("RGB", (x_pixels, y_pixels), "wheat")
         self.fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
         self.draw = ImageDraw.Draw(self.im)
@@ -327,7 +381,7 @@ class TreeImage():
     def _anti_alias(self, fill_color):
         r, g, b = fill_color
         return (r//4, g//4, b//4)
-        
+
 
 #
 # Methods to draw objects on the drawing canvas
@@ -343,8 +397,8 @@ class TreeImage():
         y1 = self.level2y(level)
 
         # Hack: drawing text twice looks better in PIL
-        self.draw.text((x1+10,y1+10), rank, font=self.fnt, fill="black")
-        self.draw.text((x1+10,y1+10), rank, font=self.fnt, fill="black")
+        self.draw.text((x1+10, y1+10), rank, font=self.fnt, fill="black")
+        self.draw.text((x1+10, y1+10), rank, font=self.fnt, fill="black")
 
 
     def draw_fiber(self, level, start_offset, end_offset, highlight=False):
@@ -358,8 +412,8 @@ class TreeImage():
         x2 = self.offset2x(end_offset) - gap
         y2 = y1 + height
         fill_color = (128,128,128) if not highlight else (233,198,109)
-        
-        self.draw.ellipse(((x1,y1), (x2,y2)), fill_color, (0, 0, 0))
+
+        self.draw.ellipse(((x1, y1), (x2, y2)), fill_color, (0, 0, 0))
 
 
     def draw_coord(self, level, offset, coord, highlight=[]):
@@ -369,8 +423,11 @@ class TreeImage():
         y1 = self.level2y(level)
         x2 = x1 + 40
         y2 = y1 + 40
-        color = "goldenrod" if len(highlight)>0 else "black"
+
+        color = "goldenrod" if len(highlight) > 0 else "black"
+
         x_text = x1+15
+
         if coord != "R" and isinstance(coord, int):
             if int(coord) >= 10:
                 x_text = x_text - 7
@@ -380,9 +437,14 @@ class TreeImage():
                 x_text = x_text - 7
 
         self.draw.ellipse(((x1,y1), (x2,y2)), color, 1)
+        #
         # Hack: drawing text twice looks better in PIL
-        self.draw.text((x_text,y1+10), str(coord), font=self.fnt, fill="white")
-        self.draw.text((x_text,y1+10), str(coord), font=self.fnt, fill="white")
+        #
+        for n in range(2):
+            self.draw.text((x_text, y1+10),
+                           str(coord),
+                           font=self.fnt,
+                           fill="white")
 
 
     def draw_value(self, level, offset, value, highlight=[]):
@@ -401,30 +463,29 @@ class TreeImage():
 
         x2 = x1 + 40
         y2 = y1 + len(value)*(font_y+10)
-        
+
         if y2 > self.max_y:
             self.max_y = y2
 
         if len(highlight) == 0:
             fill_color = self._color
-            self.draw.rectangle(((x1,y1), (x2,y2)), fill_color, 1)
+            self.draw.rectangle(((x1, y1), (x2, y2)), fill_color, 1)
         else:
             step = (y2-y1) // len(highlight)
             y1c = y1
             for worker in highlight:
                 y2c = y1c + step
                 fill_color = self.worker_color[worker]
-                self.draw.rectangle(((x1,y1c), (x2,y2c)), fill_color, 1)
+                self.draw.rectangle(((x1, y1c), (x2, y2c)), fill_color, 1)
                 y1c = y2c
 
-
-
-        for i,v in enumerate(value):
+        for i, v in enumerate(value):
             if isinstance(v, Payload):
                 v = v.value
 
             x_text = x1+15
             y_text = y1+10+(i*font_y)
+
             if (isinstance(v, int)):
                 if v >= 10:
                     x_text = x_text - 7
@@ -433,17 +494,14 @@ class TreeImage():
                 if v >= 1000:
                     x_text = x_text - 7
 
-
+            #
             # Hack: drawing text twice looks better in PIL
-            self.draw.text((x_text, y_text),
-                            str(v),
-                            font=self.fnt,
-                            fill="white")
-            self.draw.text((x_text, y_text),
-                            str(v),
-                            font=self.fnt,
-                            fill="white")
-
+            #
+            for n in range(2):
+                self.draw.text((x_text, y_text),
+                               str(v),
+                               font=self.fnt,
+                               fill="white")
 
 
     def draw_line(self, level1, offset1, level2, offset2, highlight=False):
@@ -455,9 +513,11 @@ class TreeImage():
         y1 = self.level2y(level1) + 40
         x2 = self.offset2x(offset2)
         y2 = self.level2y(level2) - 10
+
         fill_color = "goldenrod" if highlight else "black"
 
-        self.draw.line([ (x1, y1), (x2, y2) ], width=3, fill=fill_color)
+        self.draw.line([(x1, y1), (x2, y2)], width=3, fill=fill_color)
+
 
     def draw_intra_line(self, level, fiber_offset, coord_offset, highlight=False):
 
@@ -468,9 +528,10 @@ class TreeImage():
         y1 = self.level2y(level) - 10
         x2 = self.offset2x(coord_offset)
         y2 = self.level2y(level)
+
         fill_color = "goldenrod" if highlight else "black"
 
-        self.draw.line([ (x1, y1), (x2, y2) ], width=3, fill=fill_color)
+        self.draw.line([(x1, y1), (x2, y2)], width=3, fill=fill_color)
 
 #
 #
@@ -478,9 +539,10 @@ class TreeImage():
 #
     def offset2x(self, offset):
         return 200 + 80*offset
-            
+
+
     def level2y(self, level):
-        return 40+ 80*level
+        return 40 + 80*level
 
 
 if __name__ == "__main__":
