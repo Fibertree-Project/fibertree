1de13f4c6cd6378e5a70ee1106c8112e9bc08d53
Nandeeka Nayak <nandeeka@gmail.com>
2021-10-27 15:49:42 -0500

    Add Payload compute metrics tests

--- test/test_payload.py
+++ test/test_payload.py
@@ -1,5 +1,6 @@
 import unittest
 
+from fibertree import Metrics
 from fibertree import Payload
 
 class TestPayload(unittest.TestCase):
@@ -10,7 +11,7 @@ class TestPayload(unittest.TestCase):
 
         a = Payload(av)
         self.assertEqual(a.value, av)
-        
+
         b = Payload(bv)
         self.assertEqual(b.value, bv)
 
@@ -29,9 +30,32 @@ class TestPayload(unittest.TestCase):
 
         a += 1
         self.assertEqual(a.value, av+1)
-        
+
         a += b
         self.assertEqual(a.value, av+1+bv)
+
+    def test_plus_metrics(self):
+        av = 1
+        bv = 2
+
+        a = Payload(av)
+        b = Payload(bv)
+
+        Metrics.beginCollect()
+        _ = a + b
+        self.assertEqual(Metrics.dump(), {"Compute": {"payload_add": 1}})
+
+        _ = a + 2
+        self.assertEqual(Metrics.dump(), {"Compute": {"payload_add": 2}})
+
+        _ = 1 + b
+        self.assertEqual(Metrics.dump(), {"Compute": {"payload_add": 3}})
+
+        a += b
+        self.assertEqual(Metrics.dump(), {
+            "Compute": {"payload_add": 4, "data_update": 1}})
+
+        Metrics.endCollect()
 
     def test_minus(self):
         av = 1
@@ -42,7 +66,7 @@ class TestPayload(unittest.TestCase):
 
         a_sub_1 = a - 1
         self.assertEqual(a_sub_1.value, av-1)
-        
+
         a_sub_b = a - b
         self.assertEqual(a_sub_b.value, av-bv)
 
@@ -69,15 +93,38 @@ class TestPayload(unittest.TestCase):
         a *= b
         self.assertEqual(a.value, av*2*b)
 
+    def test_mul_metrics(self):
+        av = 1
+        bv = 2
+
+        a = Payload(av)
+        b = Payload(bv)
+
+        Metrics.beginCollect()
+        _ = a * b
+        self.assertEqual(Metrics.dump(), {"Compute": {"payload_mul": 1}})
+
+        _ = a * 2
+        self.assertEqual(Metrics.dump(), {"Compute": {"payload_mul": 2}})
+
+        _ = 1 * b
+        self.assertEqual(Metrics.dump(), {"Compute": {"payload_mul": 3}})
+
+        a *= b
+        self.assertEqual(Metrics.dump(), {
+            "Compute": {"payload_mul": 4, "data_update": 1}})
+
+        Metrics.endCollect()
+
     def test_equality(self):
         cv = 8
         dv = 8
         ev = 1
-        
+
         c = Payload(cv)
         d = Payload(dv)
         e = Payload(ev)
-        
+
         self.assertTrue(c == d)
         self.assertFalse(c == e)
 
