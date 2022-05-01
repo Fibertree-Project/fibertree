9d17756f1987902c236920d61266fe81641b7f36
Nandeeka Nayak <nandeeka@gmail.com>
2022-05-01 17:52:54 -0500

    Implement swapCount

--- fibertree/model/compute.py
+++ fibertree/model/compute.py
@@ -1,44 +1,126 @@
 #cython: language_level=3
 """
-YS
+Compute the number operations executed
 """
+import bisect
 
 from fibertree import Tensor
 
 class Compute:
     """YS
     """
-    def __init__(self):
-        """__init__"""
-        pass
 
     @staticmethod
     def opCount(dump, op):
-        """Compute the number of operations executed by this kernel
-        YS
-
-        Compute.opCount(Metrics.dump(), "mul") -> return # of multiplications
-        """
+        """Compute the number of operations executed by this kernel """
         metric = "payload_" + op
         if(metric in dump["Compute"].keys()):
             return dump["Compute"][metric]
         else:
             return 0
 
+    @staticmethod
     def lfCount(dump, rank, leader):
-        """Compute the number of intersection attempts 
-        
+        """
+        Compute the number of intersection attempts with leader-follower
+        intersection
+
         leader is 0 or 1 depending on which tensor the leader is.
         """
-        
+
         line = "Rank " + rank
         l = "tensor" + str(leader)
         metric = "unsuccessful_intersect_" + l
         return dump[line]["successful_intersect"] + dump[line][metric]
-        
+
+    @staticmethod
     def skipCount(dump, rank):
-    
+        """
+        Compute the number of intersection attempts with skip-ahead
+        intersection
+        """
         line = "Rank " + rank
         total = dump[line]["successful_intersect"] + dump[line]["unsuccessful_intersect_tensor0"] + dump[line]["unsuccessful_intersect_tensor1"]
         skipped = dump[line]["skipped_intersect"]
         return total - skipped
+
+    @staticmethod
+    def swapCount(tensor, depth, radix, next_latency):
+        """Compute the number of swaps required at the given depth"""
+        return Compute._swapCountTree(tensor.getRoot(), depth, radix, next_latency)
+
+    @staticmethod
+    def _swapCountTree(fiber, depth, radix, next_latency):
+        """Compute the number of swaps required at the given depth"""
+        swaps = 0
+
+        # Recurse if necessary
+        if depth > 0:
+            depth -= 1
+            for _, payload in fiber:
+                swaps += Compute._swapCountTree(payload, depth, radix, next_latency)
+            return swaps
+
+        # Otherwise merge
+        coords = []
+        for _, payload in fiber:
+            coords.append(sorted([-c for c in payload.getCoords()]))
+
+        while len(coords) > 1:
+            new = []
+            if radix > len(coords):
+                radix = len(coords)
+
+            for i in range(0, len(coords), radix):
+                end = min(i + radix, len(coords))
+                ops, merged = Compute._merge(coords[i:end], radix, next_latency)
+
+                swaps += ops
+                new.append(merged)
+
+            coords = new
+
+        return swaps
+
+    @staticmethod
+    def _merge(coords, radix, next_latency):
+        """
+        Merge sorted lists of coordinates into a single list
+
+        All coordinates are negated to work with list.pop() and bisect
+        """
+        # If we have a finite next latency, use that
+        if isinstance(next_latency, int):
+            merged = [c for list_ in coords for c in list_]
+            merged.sort()
+            return next_latency * (len(coords) + len(merged)), merged
+
+        # Otherwise, merge incrementally
+        # First get the heads
+        head = []
+        compares = 0
+
+        # First insert all fibers
+        for i, list_ in enumerate(coords):
+            elem = (list_.pop(), i)
+            j = bisect.bisect_right(head, elem)
+            compares += len(head) - j + 1
+            head.insert(j, elem)
+
+        # Now build the result
+        merged = []
+        while head:
+            elem = head.pop()
+            merged.append(elem[0])
+            if len(coords[elem[1]]) == 0:
+                continue
+
+            new = (coords[elem[1]].pop(), elem[1])
+            j = bisect.bisect_right(head, new)
+            compares += len(head) - j + 1
+
+            head.insert(j, new)
+
+        merged.sort()
+
+        return compares, merged
--- test/test_compute.py
+++ test/test_compute.py
@@ -5,7 +5,8 @@ import unittest
 from fibertree import Metrics
 from fibertree import Payload
 from fibertree import Fiber
-from fibertree.model.compute import Compute
+from fibertree import Tensor
+from fibertree.model import Compute
 
 class TestCompute(unittest.TestCase):
     """Tests of the Compute class"""
@@ -30,7 +31,7 @@ class TestCompute(unittest.TestCase):
         self.assertEqual(Compute.opCount(Metrics.dump(), "add"), 1)
 
         Metrics.endCollect()
-        
+
     def test_compute_lf(self):
         a_k = Fiber.fromUncompressed([1, 0, 3, 4, 5])
         a_k.getRankAttrs().setId("K")
@@ -43,10 +44,10 @@ class TestCompute(unittest.TestCase):
                 pass
             Metrics.incIter("M")
         Metrics.endCollect()
-        
+
         self.assertEqual(Compute.lfCount(Metrics.dump(), "K", 0), 4)
         self.assertEqual(Compute.lfCount(Metrics.dump(), "K", 1), 3)
-        
+
     def test_compute_skip(self):
         a_k = Fiber.fromUncompressed([1, 0, 0, 0, 0, 0, 0, 8, 4])
         a_k.getRankAttrs().setId("K")
@@ -59,8 +60,56 @@ class TestCompute(unittest.TestCase):
                 pass
             Metrics.incIter("M")
         Metrics.endCollect()
-        
+
         self.assertEqual(Compute.skipCount(Metrics.dump(), "K"), 3)
+
+    def test_compute_swaps_discrete_next(self):
+        """Test swapCount"""
+        coords = [0, 1]
+        payloads = [Fiber([1, 3, 5], [1, 1, 1]), Fiber([1, 2, 3], [1, 1, 1])]
+        tensor = Tensor.fromFiber(rank_ids=["M", "K"], fiber=Fiber(coords, payloads))
+
+        self.assertEqual(Compute.swapCount(tensor, 0, 100, 3), 3 * (2 + 6))
+
+    def test_compute_swaps_finite_radix(self):
+        """Test swapCount when limited by the radix"""
+        coords = [0, 1, 2]
+        payloads = [Fiber([1, 3, 5], [1, 1, 1]), Fiber([1, 2, 3], [1, 1, 1]),
+                    Fiber([0, 4], [1, 1])]
+
+        tensor = Tensor.fromFiber(rank_ids=["M", "K"], fiber=Fiber(coords, payloads))
+        ops = 3 * (2 + 6) + 3 * (1 + 2) + 3 * (2 + 8)
+
+        self.assertEqual(Compute.swapCount(tensor, 0, 2, 3), ops)
+
+    def test_compute_swaps_undefined_next(self):
+        """Test swapCount when the next_latency is N"""
+        coords = [0, 1, 2]
+        payloads = [Fiber([1, 3, 5], [1, 1, 1]), Fiber([0, 2, 3], [1, 1, 1]),
+                    Fiber([1, 4], [1, 1])]
+
+        tensor = Tensor.fromFiber(rank_ids=["M", "K"], fiber=Fiber(coords, payloads))
+        ops = (1 + 1 + 2) + (3 + 3 + 2 + 1 + 0 + 2 + 0 + 0)
+
+        self.assertEqual(Compute.swapCount(tensor, 0, float("inf"), "N"), ops)
+
+    def test_compute_swaps_depth_1(self):
+        """Test swapCount when the depth is > 0"""
+        coords_0 = [0, 1, 2]
+        payloads_0 = [Fiber([1, 3, 5], [1, 1, 1]), Fiber([0, 2, 3], [1, 1, 1]),
+                    Fiber([1, 4], [1, 1])]
+        fiber_0 = Fiber(coords_0, payloads_0)
+
+        coords_1 = [0, 1]
+        payloads_1 = [Fiber([1, 3, 5], [1, 1, 1]), Fiber([1, 2, 3], [1, 1, 1])]
+        fiber_1 = Fiber(coords_1, payloads_1)
+
+        root = Fiber([3, 7], [fiber_0, fiber_1])
+        tensor = Tensor.fromFiber(rank_ids=["M", "N", "K"], fiber=root)
+        ops = 3 * (2 + 6) + 3 * (1 + 2) + 3 * (2 + 8) + 3 * (2 + 6)
+
+        self.assertEqual(Compute.swapCount(tensor, 1, 2, 3), ops)
+
 
 if __name__ == '__main__':
     unittest.main()
