9e65c7f9f6e561d33737da34cb20ee3f0406bc44
yasmin sarita <ycs4@cornell.edu>
2022-05-01 13:28:43 -0500

    compute.py

--- fibertree/model/compute.py
+++ fibertree/model/compute.py
@@ -19,4 +19,26 @@ class Compute:
 
         Compute.opCount(Metrics.dump(), "mul") -> return # of multiplications
         """
-        pass
+        metric = "payload_" + op
+        if(metric in dump["Compute"].keys()):
+            return dump["Compute"][metric]
+        else:
+            return 0
+
+    def lfCount(dump, rank, leader):
+        """Compute the number of intersection attempts 
+        
+        leader is 0 or 1 depending on which tensor the leader is.
+        """
+        
+        line = "Rank " + rank
+        l = "tensor" + str(leader)
+        metric = "unsuccessful_intersect_" + l
+        return dump[line]["successful_intersect"] + dump[line][metric]
+        
+    def skipCount(dump, rank):
+    
+        line = "Rank " + rank
+        total = dump[line]["successful_intersect"] + dump[line]["unsuccessful_intersect_tensor0"] + dump[line]["unsuccessful_intersect_tensor1"]
+        skipped = dump[line]["skipped_intersect"]
+        return total - skipped
--- test/test_compute.py
+++ test/test_compute.py
@@ -2,7 +2,65 @@
 
 import unittest
 
+from fibertree import Metrics
+from fibertree import Payload
+from fibertree import Fiber
+from fibertree.model.compute import Compute
+
 class TestCompute(unittest.TestCase):
     """Tests of the Compute class"""
 
-    pass
+    def test_compute_op(self):
+        av = 1
+        bv = 2
+
+        a = Payload(av)
+        b = Payload(bv)
+
+        Metrics.beginCollect(["K"])
+        _ = a * b
+        self.assertEqual(Compute.opCount(Metrics.dump(), "mul"), 1)
+        self.assertEqual(Compute.opCount(Metrics.dump(), "add"), 0)
+
+        _ = a + 2
+        _ = 1 * b
+
+        a *= b
+        self.assertEqual(Compute.opCount(Metrics.dump(), "mul"), 3)
+        self.assertEqual(Compute.opCount(Metrics.dump(), "add"), 1)
+
+        Metrics.endCollect()
+        
+    def test_compute_lf(self):
+        a_k = Fiber.fromUncompressed([1, 0, 3, 4, 5])
+        a_k.getRankAttrs().setId("K")
+        b_k = Fiber.fromUncompressed([0, 0, 6, 7, 9])
+        b_k.getRankAttrs().setId("K")
+
+        Metrics.beginCollect(["M", "K"])
+        for _ in range(1):
+            for _ in Fiber.intersection(a_k, b_k):
+                pass
+            Metrics.incIter("M")
+        Metrics.endCollect()
+        
+        self.assertEqual(Compute.lfCount(Metrics.dump(), "K", 0), 4)
+        self.assertEqual(Compute.lfCount(Metrics.dump(), "K", 1), 3)
+        
+    def test_compute_skip(self):
+        a_k = Fiber.fromUncompressed([1, 0, 0, 0, 0, 0, 0, 8, 4])
+        a_k.getRankAttrs().setId("K")
+        b_k = Fiber.fromUncompressed([0, 2, 3, 4, 6, 0, 0, 4, 0])
+        b_k.getRankAttrs().setId("K")
+
+        Metrics.beginCollect(["M", "K"])
+        for _ in range(1):
+            for _ in Fiber.intersection(a_k, b_k):
+                pass
+            Metrics.incIter("M")
+        Metrics.endCollect()
+        
+        self.assertEqual(Compute.skipCount(Metrics.dump(), "K"), 3)
+
+if __name__ == '__main__':
+    unittest.main()
